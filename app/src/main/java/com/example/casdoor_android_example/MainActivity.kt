63ab3f29b2342cc5f02807560fd750f196ebacf5
yuziliang <yuziliangvictory@gmail.com>
2022-09-28 22:41:46 +0800

    feat: update example

--- app/src/main/java/com/example/casdoor_android_example/MainActivity.kt
+++ app/src/main/java/com/example/casdoor_android_example/MainActivity.kt
@@ -17,26 +17,25 @@
 package com.example.casdoor_android_example
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.view.View
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import org.casdoor.Casdoor
 import org.casdoor.CasdoorConfig
 
 class MainActivity : AppCompatActivity() {
+
     private var casdoor: Casdoor? = null
-    private var info: TextView? = null
+    private var isLogin = false
+    private var acToken = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        info = findViewById<TextView>(R.id.info)
-        val next = findViewById<Button>(R.id.start)
-        next.text = "Click me"
 
         val casdoorConfig = CasdoorConfig(
             endpoint = "https://door.casdoor.com",
@@ -50,10 +49,33 @@ class MainActivity : AppCompatActivity() {
             ActivityResultContracts.StartActivityForResult(),
             launcherCallback
         )
-        next.setOnClickListener {
-            val intent = Intent(this@MainActivity, WebViewActivity::class.java)
-            intent.putExtra("url", casdoor?.getSignInUrl());
-            resultLauncher.launch(intent)
+        tv_login.setOnClickListener {
+            if (!isLogin) {
+                val intent = Intent(this@MainActivity, WebViewActivity::class.java)
+                intent.putExtra("url", casdoor?.getSignInUrl(scope = "profile"));
+                resultLauncher.launch(intent)
+            } else {
+                pb_progress.visibility = View.VISIBLE
+                Thread {
+                    try {
+                        val logout = casdoor?.logout(acToken, null)
+                        if (logout == true) {
+                            runOnUiThread {
+                                tv_name.text = ""
+                                tv_name.visibility = View.GONE
+                                isLogin = false
+                                tv_login.text = "Login with Casdoor"
+                                pb_progress.visibility = View.GONE
+                            }
+                        }
+                    } catch (e: Exception) {
+                        runOnUiThread {
+                            pb_progress.visibility = View.GONE
+                            tv_name?.text = e.message
+                        }
+                    }
+                }.start()
+            }
         }
 
     }
@@ -62,11 +84,28 @@ class MainActivity : AppCompatActivity() {
         val data = result.data
         if (result.resultCode == RESULT_OK) {
             val code = data?.getStringExtra("code")
-            info?.text = code
+            pb_progress.visibility = View.VISIBLE
             Thread {
+                runOnUiThread {
+                    tv_name?.text = "Loading..."
+                }
                 code?.let {
-                    val token = casdoor?.requestOauthAccessToken(code)
-                    runOnUiThread { info?.text = token.toString() }
+                    try {
+                        acToken = casdoor?.requestOauthAccessToken(code)?.accessToken.toString()
+                        val userData = casdoor?.getUserInfo(acToken)
+                        runOnUiThread {
+                            tv_name.text = userData?.name
+                            tv_name.visibility = View.VISIBLE
+                            isLogin = true
+                            tv_login.text = "Logout"
+                            pb_progress.visibility = View.GONE
+                        }
+                    } catch (e: Exception) {
+                        runOnUiThread {
+                            pb_progress.visibility = View.GONE
+                            tv_name?.text = e.message
+                        }
+                    }
                 }
             }.start()
 
